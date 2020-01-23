059ef4cb9551a27e952616bd3c2ff582212f0622
georgema <570388779@qq.com>
2020-01-23 18:39:16 +0800

    login use cookie and session

--- src/main/java/com/demo/comunity/controller/AuthrizeController.java
+++ src/main/java/com/demo/comunity/controller/AuthrizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthrizeController {
 
@@ -26,7 +28,8 @@ public class AuthrizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name= "code") String code,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
@@ -36,7 +39,13 @@ public class AuthrizeController {
 
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user != null){
+            //登录成功，编写cookie和session
+            request.getSession().setAttribute("user",user);
+            return "redirect:index";
+        }else{
+            //登录失败，重新登录
+            return "redirect:index";
+        }
     }
 }
